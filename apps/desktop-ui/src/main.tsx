8f2d89b61f11d46ec0cb949e1675481c4df291a2
Yash Khatri <yashkhatri9950739369@gmail.com>
2026-07-18 04:58:13 +0000

    Add Vercel Analytics and Speed Insights

--- apps/desktop-ui/src/main.tsx
+++ apps/desktop-ui/src/main.tsx
@@ -1,24 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import "./index.css";
-
 import App from "./App";
-
 import { SidebarProvider } from "./contexts/SidebarContext";
 
-createRoot(
-  document.getElementById("root")!
-).render(
-
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-
     <SidebarProvider>
-
       <App />
-
+      <Analytics />
+      <SpeedInsights />
     </SidebarProvider>
-
   </StrictMode>
-
 );
